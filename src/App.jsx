74a60c4e674cdac676e574254318a54914eedf23
kiethohoai <kietho.060193@gmail.com>
2024-06-29 14:31:32 +0700

    006 Prop Drilling

--- src/App.jsx
+++ src/App.jsx
@@ -52,21 +52,22 @@ const average = (arr) =>
 
 // todo: App()
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar movies={movies} />
+      <Main movies={movies} />
     </>
   );
 }
 
 // todo: NavBar()
-function NavBar() {
+function NavBar({ movies }) {
   return (
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <NumResults />
+      <NumResults movies={movies} />
     </nav>
   );
 }
@@ -96,27 +97,27 @@ function Search() {
 }
 
 // todo: NumResults()
-function NumResults() {
+function NumResults({ movies }) {
   return (
     <p className="num-results">
-      {/* Found <strong>{movies.length}</strong> results */}
-      Found <strong>"X"</strong> results
+      Found <strong>{movies.length}</strong> results
+      {/* Found <strong>"X"</strong> results */}
     </p>
   );
 }
 
 // TODO: Main()
-function Main() {
+function Main({ movies }) {
   return (
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies} />
       <WatchedBox />
     </main>
   );
 }
 
 // todo ListBox()
-function ListBox() {
+function ListBox({ movies }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -128,14 +129,15 @@ function ListBox() {
         {isOpen1 ? "–" : "+"}
       </button>
 
-      {isOpen1 && <MovieList />}
+      {isOpen1 && <MovieList movies={movies} />}
     </div>
   );
 }
 
 // todo MovieList()
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({ movies }) {
+  // const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <ul className="list">
       {movies?.map((movie, i) => (
