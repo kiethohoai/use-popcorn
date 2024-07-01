7caf7990898394827eb8c5a604897559660f18ee
kiethohoai <kietho.060193@gmail.com>
2024-07-01 10:18:52 +0700

    004 useEffect to the Rescue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -58,9 +58,11 @@ export default function App() {
   const [watched, setWatched] = useState([]);
   const KEY = "6d431fb9";
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then((res) => res.json())
-    .then((data) => console.log(data.Search));
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
 
   return (
     <>
